870dca4dec5bf95a0a05ad907267535744200829
USER <USER@DESKTOP-9J7QU01>
2018-03-20 21:22:58 +0200

    shtok

--- hwsOOP/src/hw1/Rectangle1.java
+++ hwsOOP/src/hw1/Rectangle1.java
@@ -1,5 +1,22 @@
 package hw1;
 
-public class Rectangle1 {
 
+public class Rectangle1 implements Rectangle {
+	Point top_right;
+	Point bottom_left;
+	
+	public Rectangle()
+	{
+		this.top_right = new Point(0.5,0.5);
+		this.bottom_left = new Point(-0.5,-0.5);
+	}
+	public Rectangle(point top Point bottom)
+	{
+		this();
+		if(this.top_right.isUpper(this.bottom_left)&&this.top_right.isRight(this.bottom_left))
+		{
+			this.top_right = top.copy();
+			this.bottom_left = bottom.copy();
+		}
+	}
 }
